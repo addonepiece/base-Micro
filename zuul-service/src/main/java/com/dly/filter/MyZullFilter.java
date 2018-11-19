fd600b971ba135914f7774a62fe8549d02bd7850
yanxinrong <yan.xinrong@icerno.com>
2018-11-19 17:03:21 +0800

    zull代理过滤器实例

---
+++ zuul-service/src/main/java/com/dly/filter/MyZullFilter.java
@@ -0,0 +1,48 @@
+package com.dly.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyZullFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    // http://localhost:8762/cms-api/hi?name=xxx&token="123"
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            System.out.println(String.format("token is empty"));
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        System.out.println(String.format("ok"));
+        return null;
+    }
+
+}
