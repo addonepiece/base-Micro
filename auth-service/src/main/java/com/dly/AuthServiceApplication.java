f08fb8a95770fe14075def9c65103a22c034ea8c
yanxinrong <yan.xinrong@icerno.com>
2018-11-26 10:40:34 +0800

    将zuul-server module 改名为zuul-service

--- auth-service/src/main/java/com/dly/AuthServiceApplication.java
+++ auth-service/src/main/java/com/dly/AuthServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServerApplication {
+public class AuthServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthServerApplication.class, args);
+        SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
