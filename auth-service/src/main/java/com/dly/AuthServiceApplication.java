01010601689857b72d882cac2581fbc1195faaf2
yanxinrong <yan.xinrong@icerno.com>
2018-12-28 16:17:32 +0800

    pom文件jar包依赖调整

--- auth-service/src/main/java/com/dly/AuthServiceApplication.java
+++ auth-service/src/main/java/com/dly/AuthServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 @RestController
 @EnableSwagger2
 public class AuthServiceApplication {
